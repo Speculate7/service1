bc80150a3ddd73bad730e20c3aa850f877fe139e
speculate7 <ore.oladuni@gmail.com>
2020-06-09 00:54:59 -0400

    restructering and cleanup

---
+++ build/_dist_/index.js
@@ -0,0 +1,24 @@
+const extraHtml = `
+<div class="container externalHtml">
+<h3>Html injection experiment</h3>
+</div>`;
+
+const importHtmlPage = () => {
+  const importCheck = () => {
+    if ("import" in document.createElement("link")) {
+      // This browser supports HTML5 Imports.
+      return true;
+    }
+
+    return false;
+  };
+
+  console.log(importCheck()); //clean the html using DOMPurify and setto the value of a new elem.
+
+  let elem = document.createElement("div");
+  elem.innerHTML = extraHtml;
+  console.log(elem);
+  document.body.appendChild(elem);
+};
+
+importHtmlPage();
